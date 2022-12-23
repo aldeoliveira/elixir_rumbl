7f665172cd9a03d4768d556d09a2e9ea1f9d01da
André <aldeoliveira@gmail.com>
2022-12-23 15:51:47 -0300

    add login and logout

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -8,4 +8,14 @@ defmodule RumblWeb.Auth do
     user = user_id && Rumbl.Accounts.get_user(user_id)
     assign(conn, :current_user, user)
   end
+
+  @doc """
+  Receives the connection and the user, and stores the user ID in the section.
+  """
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true)
+  end
 end
